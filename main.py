826499101c024d7ef9e50ef4abeff58809fa0632
Karim Lalani <jimmy00784@gmail.com>
2023-10-29 12:10:04 -0500

    added documentation for mac and gpu

--- main.py
+++ main.py
@@ -33,18 +33,25 @@ def create_chain(system_prompt):
     # callback_manager = CallbackManager([stream_handler])
 
     (repo_id, model_file_name) = ("TheBloke/Mistral-7B-Instruct-v0.1-GGUF",
-                                  "mistral-7b-instruct-v0.1.Q5_0.gguf")
+                                  "mistral-7b-instruct-v0.1.Q4_0.gguf")
 
     model_path = hf_hub_download(repo_id=repo_id,
                                  filename=model_file_name,
                                  repo_type="model")
 
+    # initialize LlamaCpp llm model
+    # n_gpu_layers, n_batch, and n_ctx are for GPU support.
+    # When not set, CPU will be used.
+    # set 1 for mac m2, and higher numbers based on your GPU support
     llm = LlamaCpp(
             model_path=model_path,
             temperature=0,
             max_tokens=512,
             top_p=1,
             # callback_manager=callback_manager,
+            # n_gpu_layers=1,
+            # n_batch=512,
+            # n_ctx=4096,
             verbose=False,
             streaming=True,
             )
